e00fe340a1dfe42699c0eb9fe027bfd99ea86466
wjlawlor <wjl28@msn.com>
2019-09-12 15:36:06 -0400

    Working community pages.

---
+++ CommunityToolShedMvc/Controllers/CommunityController.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolShedMvc.Data;
+using CommunityToolShedMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityToolShedMvc.Controllers
+{
+    public class CommunityController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Community> communities = DatabaseHelper.Retrieve<Community>(@"
+                SELECT Community.ID, Community.Name, Community.OwnerID, CONCAT(Person.FirstName,' ',Person.LastName) AS OwnerName, 
+                    Community.TypeID, CommunityType.Type AS TypeName
+                FROM Community
+                JOIN CommunityType ON Community.TypeID = CommunityType.ID
+                JOIN Person ON Community.OwnerID = Person.ID");
+
+            return View(communities);
+        }
+
+        public ActionResult Overview(int id)
+        {
+            Community community = DatabaseHelper.RetrieveSingle<Community>(@"
+                    SELECT Community.ID, Community.Name, Community.OwnerID, CONCAT(Person.FirstName,' ',Person.LastName) AS OwnerName, 
+                        Community.TypeID, CommunityType.Type AS TypeName
+                    FROM Community
+                    JOIN CommunityType ON Community.TypeID = CommunityType.ID
+                    JOIN Person ON Community.OwnerID = Person.ID
+                    WHERE Community.ID = @ID
+                ",
+                    new SqlParameter("@ID", id)
+                );
+
+            return View(community);
+        }
+    }
+}
---
+++ CommunityToolShedMvc/Models/Community.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityToolShedMvc.Models
+{
+    public class Community
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int OwnerID { get; set; }
+        public string OwnerName { get; set; }
+        public int TypeID { get; set; }
+        public string TypeName { get; set; }
+
+        // Maybe
+        // public List<int> MemberList { get; set; }
+    }
+}
